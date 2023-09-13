ff17308efe2c9fe5d10c70fc4d5c3d3ef79b3421
cliff-omega <clifford@omega-accounting.com>
2023-09-13 14:05:01 -0700

    alchemy placeholder

--- azureSqlConnector.py
+++ azureSqlConnector.py
@@ -2,20 +2,23 @@ import os
 import urllib.parse 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
+
+# # CREATE DATABASE CONNECTION URI  
+# connectionUri = urllib.parse.quote_plus("Driver={ODBC Driver 18 for SQL Server};Server=tcp:sql-oas.database.windows.net,1433;Database=Teamwork;Uid=oasacctadmin;Pwd=VQAhZMbQKWYjS4M;Encrypt=yes;TrustServerCertificate=no;")
 
 
-# CREATE DATABASE CONNECTION URI  
-connectionUri = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=sqlhost.database.windows.net;DATABASE=pythonSQL;UID=username@sqldb;PWD=password56789")
+# # INITIALIZE FLASK APP AND CONFIGURE CONNECTION TO SQL SERVER
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'supersecret'
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % connectionUri
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+# # STORE APP AND ITS CONFIGURED SQL CONNECTION AS A VARIABLE
+# sqlDb = SQLAlchemy(app)
 
-# INITIALIZE FLASK APP AND CONFIGURE CONNECTION TO SQL SERVER
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % connectionUri
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-# STORE APP AND ITS CONFIGURED SQL CONNECTION AS A VARIABLE
-sqlDb = SQLAlchemy(app)
+# session = Session(engine)
+# selectQuery = select(dbo.Comments).limit(5).all()
 
 def connectAndWriteToAzureSql(contentsToWrite):
     print('connectAndWriteToAzureSql function was passed the following parameters:')
